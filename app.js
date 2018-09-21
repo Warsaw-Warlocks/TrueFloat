530ebb237086b7d98ce3b1c4becdd126f385a4ef
krystian nowak <krystian.nowak@tedd.berlin>
2018-09-21 08:46:43 +0200

    adding temp multiplyTF

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // two TF (true float)
 // operations: + - * /
-var hello = 12;
 
 function TF(number) {
 
@@ -33,7 +32,39 @@ function addTF(num1, num2) {
     })
 }
 
-let num1 = new TF(13);
-let num2 = new TF(9.0003005);
+function multiplyTF(num1, num2) {
+    // const leftSide = num1.leftSide * num2.leftSide;
+    const power = num1.power * num2.power;
+    // // const rightSide = num1.rightSide * num2.power * num2.rightSide * num1.power;
+    // const rightSide = num1.rightSide * num2.rightSide;
+
+    const temp1 = num1.leftSide * num2.leftSide;
+    const temp2 = num1.rightSide / num2.power * num2.leftSide;
+    const temp3 = num1.leftSide * num2.rightSide / num1.power;
+    const temp4 = num1.rightSide / num2.power * num2.rightSide / num1.power;
+
+    const temp1A = temp1.toString().split(".");
+    const temp2A = temp2.toString().split(".");
+    const temp3A = temp3.toString().split(".");
+    const temp4A = temp4.toString().split(".");
+
+    const leftSide = (parseInt(temp1A[0]) + parseInt(temp2A[0]) + parseInt(temp3A[0]) + parseInt(temp4A[0]));
+    const rightSide = (parseInt((temp1A[1] || "0") + parseInt(temp2A[1]) + parseInt(temp3A[1]) + parseInt(temp4A[1])));
+
+    console.log(temp1, temp2, temp3, temp4);
+    console.log(temp1A, temp2A, temp3A, temp4A);
+    console.log(leftSide, rightSide);
+
+    return new TF({
+        leftSide,
+        rightSide,
+        power,
+    })
+}
+
+let num1 = new TF(1.5);
+let num2 = new TF(1.5);
 let num3 = addTF(num1, num2);
-console.log(num1, num2, num3);
+let num4 = multiplyTF(num1, num2);
+// console.log(num1, num2, num3);
+console.log(num1, num2, num4);
